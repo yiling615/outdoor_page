a87e7edbe98c93113f5ed02aebaf3bf5941f7113
ben_29 <ben_29@live.com>
2025-09-01 17:40:06 +0800

    style: fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -425,7 +425,7 @@ const Index = () => {
           />
         )}
       </div>
-      <div className="w-full lg:w-2/3" id="map-container">
+      <div className="w-full lg:w-3/4" id="map-container">
         <RunMap
           title={title}
           viewState={viewState}
